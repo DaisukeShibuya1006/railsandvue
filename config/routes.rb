467b4403a5b4b51f8d98e9bbdf9c5f02a5ef8134
DaisukeShibuya1006 <dshibuya1006@gmail.com>
2020-05-20 22:23:33 +0900

    add users/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
+  get "users/index" => "users#index"
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
-  # devise_scope :user do
-  #   get '/users/sign_out' => 'devise/sessions#destory'
-  # end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, :only => [:show]
 end
